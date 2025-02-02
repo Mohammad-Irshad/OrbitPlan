737dcde2447c99735f57fb680c5c0a36bf231c97
Mohammad-Irshad <mohammadirshadcse@gmail.com>
2025-02-02 12:41:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,26 +1,35 @@
-const express = require("express")
-const cors = require("cors")
-const { initializeDatabase } = require("./database/db")
-const verifyJWT = require("./middleware/auth.middleware")
-const { userSignUp, userLogIn, userDetails, allOwners } = require("./controllers/user.controller")
-const {addNewTask, getTasks, updateTask, deleteTask} = require("./controllers/tasks.controller")
-const { addTeam, getTeams } = require("./controllers/team.controller")
-const { addProject, getProjects } = require("./controllers/project.controller")
-const { addTag, getTag } = require("./controllers/tag.controller")
+const express = require("express");
+const cors = require("cors");
+const { initializeDatabase } = require("./database/db");
+const verifyJWT = require("./middleware/auth.middleware");
+const {
+  userSignUp,
+  userLogIn,
+  userDetails,
+  allOwners,
+} = require("./controllers/user.controller");
+const {
+  addNewTask,
+  getTasks,
+  updateTask,
+  deleteTask,
+} = require("./controllers/tasks.controller");
+const { addTeam, getTeams } = require("./controllers/team.controller");
+const { addProject, getProjects } = require("./controllers/project.controller");
+const { addTag, getTag } = require("./controllers/tag.controller");
 
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
-
-initializeDatabase()
+initializeDatabase();
 
 // Welcome Api
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Mohammad Irshad's Server ! Happy Coding.")
-})
+  res.send("Welcome to Mohammad Irshad's Server ! Happy Coding.");
+});
 
 // User Api
 app.post("/auth/signup", userSignUp); // User signup
@@ -28,26 +37,25 @@ app.post("/auth/login", userLogIn); // User Login
 app.get("/auth/me", verifyJWT, userDetails); // Get user details
 
 // All user api
-app.get("/allOwners", verifyJWT, allOwners)
+app.get("/allOwners", verifyJWT, allOwners);
 
 // Task Api
-app.post("/tasks",verifyJWT, addNewTask); // create a new task
-app.get("/tasks",verifyJWT, getTasks); // Fetch task with filtering 
-app.post("/tasks/:id",verifyJWT, updateTask); // update a task
-app.delete("/tasks/:id",verifyJWT, deleteTask); // delete a task
+app.post("/tasks", verifyJWT, addNewTask); // create a new task
+app.get("/tasks", verifyJWT, getTasks); // Fetch task with filtering
+app.post("/tasks/:id", verifyJWT, updateTask); // update a task
+app.delete("/tasks/:id", verifyJWT, deleteTask); // delete a task
 
 // Team Api
-app.post("/teams",verifyJWT, addTeam); // add a new team
-app.get("/teams",verifyJWT, getTeams); // Fetch a list of teams
+app.post("/teams", verifyJWT, addTeam); // add a new team
+app.get("/teams", verifyJWT, getTeams); // Fetch a list of teams
 
 // Project Api
-app.post("/projects",verifyJWT, addProject); // add a new project.
-app.get("/projects",verifyJWT, getProjects); // Get all projects
+app.post("/projects", verifyJWT, addProject); // add a new project.
+app.get("/projects", verifyJWT, getProjects); // Get all projects
 
 // Tag Api
-app.post("/tags",verifyJWT, addTag); // Add new tags
-app.get("/tags",verifyJWT, getTag); // get all tags
-
+app.post("/tags", verifyJWT, addTag); // Add new tags
+app.get("/tags", verifyJWT, getTag); // get all tags
 
 // const newUserData = {
 //     name : "King Khan",
@@ -67,7 +75,8 @@ app.get("/tags",verifyJWT, getTag); // get all tags
 
 // addUser(newUserData)
 
+const PORT = process.env.PORT || 3000;
 
-
-
-module.exports = {app}
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT : ${PORT}`);
+});
